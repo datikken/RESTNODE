a9587f8b4a8a529447b0ac18925e288b59ef0ba6
tikken <big_nick@gmail.com>
2019-12-08 12:39:39 +0300

    initial

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const port = process.env.PORT || 5000;
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Nodemon API!');
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});

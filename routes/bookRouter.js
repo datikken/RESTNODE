0f795c86425cc2162238a0c358e85521b1496dbc
tikken <big_nick@gmail.com>
2019-12-08 15:04:47 +0300

    booksController

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,33 +1,18 @@
 const express = require('express');
+const booksController = require('../controllers/booksController');
 
 function routes(Book) {
   const bookRouter = express.Router();
+  const controller = booksController(Book);
 
   bookRouter.route('/books')
-    .post((req, res) => {
-      const book = new Book(req.body);
-
-      book.save();
-      return res.status(201).json(book);
-    })
-
-    .get((req, res) => {
-      const query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      Book.find(query, (err, books) => {
-        if (err) {
-          return res.send(err);
-        }
-        return res.json(books);
-      });
-    });
+    .post(controller.post)
+    .get(controller.get)
 
   bookRouter.use('/books/:bookId', (req, res, next) => {
       Book.findById(req.params.bookId, (err, book) => {
           if(err) {
-              return res.send(err);
+            return res.send(err);
           }
 
           if(book) {
@@ -63,8 +48,9 @@ function routes(Book) {
          Object.entries(req.body).forEach(item => {
            const key = item[0];
            const value = item[1];
+
            book[key] = value;
-         })
+         });
 
         req.book.save((err) => {
           if(err) {
